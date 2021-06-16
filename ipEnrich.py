9d368ed79c3d0fb0fd7486edd62ed235be13b3e0
EasyMetaData <ddym@redrocktx.com>
2021-06-15 21:32:04 -0500

    Update ipEnrich.py
    Speed is orders of magnitude faster.
    -Refactored code
    -Added multithreading using joblib
    -Various other updates

--- ipEnrich.py
+++ ipEnrich.py
@@ -1,50 +1,37 @@
 #!/usr/env python
 # Developed by David Dym @ easymetadata.com 
-# 07/05/2020 Rewrite for python3
 # 11/25/2020 Rewrite to make lists into separate yml. Add update to list based on age
-# 02/23/2021 Ignore ssl warnings
-# 02/25/2021 Add -o export to file option
-# 02/28/2021 Add api lookup for entries not found in M*xmind ASN db
+# 06/15/2021 Implemened joblib for faster updating and processing. Cleaned up output with quoting. Refactored code.
 import os
-import time
-import datetime
 import sys
-import io
-import pickle
+import datetime
+import multiprocessing
+from joblib import Parallel, delayed
+import json
 import socket
 import pandas as pd
-import json
 import yaml
 from urllib.parse import urlparse
 from pathlib import Path
 import geoip2.database
 import requests
-from urllib3.exceptions import InsecureRequestWarning
 from netaddr import IPNetwork, IPAddress
 import argparse
+import warnings
 
-# Suppress only the single warning from urllib3 needed.
-requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-
+_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+getFQDN = False
+bHitsOnly = False
+bSkipFeeds = False
+update_interval = 86400
 fqdn_cache = {}
-
 dicLists = {}
-
-lstResults = []
+dictASNs = {}
 
 def getFeedsFromYml():
     with open('lists.yml', 'r') as file:
         documents = yaml.full_load(file)
     return documents
-
-def writeFQDNcache():
-    with open('fqdnCache.txt', 'wb') as handle:
-        pickle.dump(fqdn_cache, handle)
-
-def loadFQDNcache():
-    if os.path.exists("fqdnCache.txt"):
-        with open('fqdnCache.txt', 'rb') as handle:
-            fqdn_cache = pickle.loads(handle.read())
 
 def CheckFQDN(ip):
     hostdns = ""
@@ -54,37 +41,38 @@ def CheckFQDN(ip):
         else:
             data = socket.gethostbyaddr(ip)
             if repr(data[0]):
-                hostdns = repr(data[0])
-            else:
-                hostdns = "NA"
-            
-            fqdn_cache[ip]=hostdns
+                hostdns = "\"{}\"".format(repr(data[0]).replace('\'',''))
+
+                 #update cache
+                fqdn_cache[ip] = hostdns
     except:
         hostdns = ""
     return hostdns
 
 def getGeoInfo(IP):
-    result = []
+    result = [] 
 
     try:
-        with geoip2.database.Reader('GeoLite2-City.mmdb') as reader:
-            responseCity = reader.city(IP)
+        with geoip2.database.Reader('GeoLite2-City.mmdb') as readCity:
+            responseCity = readCity.city(IP)
 
-            if(responseCity.city.name):
-                result.append(responseCity.city.name)
-            else:
-                result.append("\"\"")
-            if(responseCity.country.name):
-                result.append(responseCity.country.name)
-            else:
-                result.append("\"\"")
-    except:
-        result.append("\"\"")
+        if(responseCity.city.name):
+            result.append("\"%s\"" % responseCity.city.name)
+        else:
+            result.append("\"\"")
+        if(responseCity.country.name):
+            result.append("\"%s\"" % responseCity.country.name)
+        else:
+            result.append("\"\"")
+    except Exception as e: print(e)
+    #except:
+    #    result.append("\"\"")
 
     try:
-        with geoip2.database.Reader('GeoLite2-ASN.mmdb') as reader:
-            responseASN = reader.asn(IP)
+        with geoip2.database.Reader('GeoLite2-ASN.mmdb') as readASN:
+            responseASN = readASN.asn(IP)
         try:
+                #result.append("\"%s\"" % responseASN.autonomous_system_number)
                 result.append("\"%s\"" % responseASN.autonomous_system_number)
         except:
             result.append("\"\"")
@@ -93,11 +81,13 @@ def getGeoInfo(IP):
                     result.append("\"%s\"" % responseASN.autonomous_system_organization.replace(","," "))
                 else:
                     result.append("\"\"")
-        except:
-                result.append("\"\"")
+        except Exception as e: print(e)
+        #except:
+        #        result.append("\"\"")
     except:
         try:
             result.append("\"[not found]\",\"%s\"" % GetOrgInfoFallback(IP) )
+            #result.append("(MaxMind failed)" )
         except:
             result.append("\"[not found]\",\"\"")
 
@@ -105,38 +95,57 @@ def getGeoInfo(IP):
 
 #This works as a fallback when we don't find an ASN for an IP in the MaxM*nd ASN db
 def GetOrgInfoFallback(strQuery): 
-    strUrl = "http://ip-api.com/json/" + strQuery
-    r = requests.get(strUrl)
-    return r.json()['org']
+    try:
+        strUrl = "http://ip-api.com/json/" + strQuery
+        r = requests.get(strUrl)
+        return r.json()['org']
+    except Exception as e: print(e)
+    return ""
 
-def GetFireHoleLists(outFilename,strUrl,fList):   
-    my_file = Path(outFilename)
-    if not my_file.exists():
-        print("Fetching firehole list: " + outFilename)
-        r = requests.get(strUrl, verify=False, allow_redirects=True)
-        fireholeraw = r.content.decode()
+def GetFireHoleLists(skip_update, itm): 
+    warnings.filterwarnings('ignore', message='Unverified HTTPS request')
 
-        with open(outFilename,'w') as outFHfile:
-            for i, line in enumerate(fireholeraw):
-                outFHfile.write(line)
-    else:
-        #check age of file 
-        today = datetime.datetime.today()
-        modified_date = datetime.datetime.fromtimestamp(os.path.getmtime(my_file))
-        duration = today - modified_date
-        if(duration.total_seconds() > 86400):
-            print("Feed '" + outFilename + "' older than 24 hours... refreshing.")
-            os.remove(my_file)
-            GetFireHoleLists(outFilename,strUrl,fList)
+    try:
+        strUrl = urlparse(itm['url'])
+        outFilename=(os.path.basename(strUrl.path))
+        my_file = Path(outFilename)
 
-    with open(outFilename,'r') as inF:
-        #fList.clear()
-        tList = []
-        for line in inF:
-            if not line.startswith("#",0,1):
-                tList.append(line.rstrip())
+        if not skip_update:
+            if not my_file.exists():
+                r = requests.get(itm['url'], verify=False, allow_redirects=True)
+                fireholeraw = r.content.decode()
+                print("DL new list: %s" % outFilename)
+                with open(outFilename,'w', encoding='utf-8') as outFHfile:
+                    for i, line in enumerate(fireholeraw):
+                        outFHfile.write(line)
+            else:
+                #Check age of file 
+                today = datetime.datetime.today()
+                modified_date = datetime.datetime.fromtimestamp(os.path.getmtime(my_file))
+                duration = today - modified_date
+                if(duration.total_seconds() > update_interval):
+                    os.remove(my_file)
+                    GetFireHoleLists(skip_update, itm)
+    except Exception as e: print(e)
 
-        dicLists[fList] = list(set(tList))
+def LoadFeeds(documents):
+    for item, doc in documents.items():
+        if "netset" in item or "ipset" in item:
+            for itm in doc:
+                a = urlparse(itm['url'])
+                fname=(os.path.basename(a.path))
+                with open(fname,'r', encoding='utf-8') as inF:
+                    tList = []
+                    for line in inF:
+                        if not line.startswith("#",0,1):
+                            tList.append(line.rstrip())
+                dicLists[itm['name']] = list(set(tList))
+
+def setupFeeds(check_update,documents):
+    for item, doc in documents.items():
+        if "netset" in item or "ipset" in item:
+            iUpdateHrs = update_interval/60/60
+            Parallel(n_jobs=multiprocessing.cpu_count())(delayed(GetFireHoleLists)(check_update,itm) for itm in doc)
 
 def CheckFireHoleList_cidr(ip,flist,strMessage):
     #https://stackoverflow.com/questions/819355/how-can-i-check-if-an-ip-is-in-a-network-in-python-2-x
@@ -146,6 +155,12 @@ def CheckFireHoleList_cidr(ip,flist,strMessage):
                 return strMessage
         except:
             print(ncidr)
+    return ""
+
+def CheckASN(sASN):
+    for kLstName, vLstValues in dicLists.items():
+        if sASN in vLstValues:
+                return kLstName + "|"
     return ""
 
 def Checkcidr(ip, lst):
@@ -159,80 +174,88 @@ def Checkcidr(ip, lst):
             print("errorrrrr")
     return False
 
-def ipProcess(ip,args):
-    
-    row = ip + ","
-    row += getGeoInfo(ip)
+#need to return row/rows here then add after exiting the thread!
+def ipProcess(_ip):
 
-    if args.FQDN:
-        row += ", " + CheckFQDN(ip.rstrip())
-    
-    fhresult = ""
+    try:
+        ip = _ip.rstrip()
 
-    for kLstName, vLstValues in dicLists.items():
-        if ip in vLstValues:
-                fhresult += kLstName + "|"
+        row = "\"" + ip + "\","
+        row += getGeoInfo(ip)
 
-    #If we have hits from feeds clean up the end
-    if fhresult:
-        row += "," + fhresult.rstrip("|")
+        if getFQDN:
+            row += ", " + CheckFQDN(ip.rstrip())
 
-    if fhresult and args.bHitsOnly:
-        lstResults.append(row)
-        #print (row)
-    else:
-        if not args.bHitsOnly:
-            lstResults.append(row)
-            #print (row)
+        #Lookup from IP lists. Can be slow with big list
+        if bSkipFeeds:
+            fhresult = ""
+            for kLstName, vLstValues in dicLists.items():
+                if ip in vLstValues:
+                    fhresult += kLstName + "|"
 
-def setupFeeds(documents):
-    for item, doc in documents.items():
-        #if "netset" in item:
-        for itm in doc:
-            a = urlparse(itm['url'])
-            fname=(os.path.basename(a.path))
-            GetFireHoleLists(fname,itm['url'],itm['name'])
+            #If we have hits from feeds clean up the end
+            if fhresult:
+                row += ",\"" + fhresult.rstrip("|") +"\""
 
-def main():
+            if bHitsOnly and not fhresult:
+                return ""
+
+    except Exception as e: print(e)
+
+    return row
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='A tool to gather information garding IPs')
     parser.add_argument("-f", "--file", dest='file', type=str, required=False, help="File with list of IPs one per line")
-    parser.add_argument("-i", "--i", dest='ip', type=str, required=False, help="IPs to lookup")
+    parser.add_argument("-i", "--i", dest='ip', type=str, required=False, help="Lookup single IP")
     parser.add_argument("-n", "--HitsOnly", dest='bHitsOnly', required=False,action='store_true', help="Only show hits from threat feeds 'True'")
+    parser.add_argument("-j", "--SkipFeeds", dest='bSkipFeeds', required=False,action='store_false', help="Skip threat feed matching")
     parser.add_argument("-r", "--FQDN", dest='FQDN', required=False,action='store_true', help="Resolve FQDN. Provide 'True'")
-    parser.add_argument("-o", "--outfile", dest='outfile', required=False, help="Output results to a file")
-
+    parser.add_argument("-o", "--outfile", dest='outfile', required=False, help="Output results to a file in CSV")
+    parser.add_argument("-s", "--skip_update", dest='skip_update', required=False,action='store_true', help="I'm in a hurry.. Skip downloading updated lists.")
+    
     args = parser.parse_args()
 
-    loadFQDNcache()
+    getFQDN = args.FQDN
+    bHitsOnly = args.bHitsOnly
+    bSkipFeeds = args.bSkipFeeds
 
     documents = getFeedsFromYml()
 
-    setupFeeds(documents)
-    print("\n")
+    if bSkipFeeds:
+        print("Fetching new and updated feeds... [Update older than 24 hrs]")
+        setupFeeds(args.skip_update,documents)
+        LoadFeeds(documents)
+
+    if getFQDN:
+        print("Note: Hostname lookups will increase processing time.")
 
     lstColumns = ["IP, City, Country, ASN, ASN Org, FQDN, Indicators"]
-    #lstResults.append(lstColumns)
-    #print("\",\"".join(lstColumns) + "\"")
+
+    lstResults = []
 
     if args.file:
-        with open(args.file, "r") as f:
-            for ip in f:
-                ipProcess(ip.rstrip(),args)
+        with open(args.file, "r", encoding='utf-8') as f:
+            lstResults = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(ipProcess)(ip) for ip in f)
     elif args.ip:
-        ipProcess(args.ip.rstrip(),args)
+        lstResults.append(ipProcess(args.ip.rstrip()))
     else:
         print("Provide an ip or file to process...")
 
-    #write output file
+    #Remove skipped lines that didn't have threat feed hits
+    if(bHitsOnly):
+        lstResults = [i for i in lstResults if i]
+
+    #Output results
+    print ("\r\n")
+    print("\r\n".join(lstColumns))
+    print("\r\n".join(lstResults))
+    
+    #Write results to file
     if args.outfile:
-        with open(args.outfile, 'w') as f:
+        with open(args.outfile, 'w', encoding='utf-8') as f:
             print("\n".join(lstColumns), file=f)
             print("\n".join(lstResults), file=f)
-    else:
-        print("\r\n".join(lstColumns))
-        print("\r\n".join(lstResults))
-    # save fqdn cache
-    writeFQDNcache()
+        print("Results written to %s", args.outfile)
 
-if True:
-    main()
+print ("\n done")
